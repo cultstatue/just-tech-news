de66c98aafd0f86b7b507f5b764255779e3686b9
cultstatue <saltedchurros@gmail.com>
2022-08-13 18:07:05 -0400

    added hashed password creation

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -58,10 +58,12 @@ router.put('/:id', (req, res) => {
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
     })
+  
       .then(dbUserData => {
         if (!dbUserData[0]) {
           res.status(404).json({ message: 'No user found with this id' });
