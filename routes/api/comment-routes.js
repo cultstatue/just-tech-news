9e60efc5afc9b5bf701e1dedd0b4e125aaf20bc8
cultstatue <saltedchurros@gmail.com>
2022-08-14 12:55:23 -0400

    yet again

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Comment } = require('../../Models');
+const { Comment } = require('../Models');
 
 router.get('/', (req, res) => {
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote, Comment } = require("../../Models");
+const { User, Post, Vote, Comment } = require("../Models");
 
 // GET /api/users
 router.get('/', (req, res) => {
